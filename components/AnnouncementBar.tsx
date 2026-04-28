1f6b7df85e3b4ca98fa4d8f153f5bea3241b5a5a
daud157 <daudahmed157@gmail.com>
2026-04-29 00:42:26 +0500

    update2

--- components/AnnouncementBar.tsx
+++ components/AnnouncementBar.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link";
-
-import { Container } from "@/components/Container";
-import { IconStars } from "@/components/icons/HomeIcons";
-import { WHATSAPP_LINK } from "@/lib/site";
 import { cn } from "@/lib/cn";
 
 export function AnnouncementBar() {
